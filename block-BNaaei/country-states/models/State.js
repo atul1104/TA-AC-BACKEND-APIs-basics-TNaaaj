8257f5221350175f253f28ba2b4fe58cf871cef7
atul1104 <waghamareatul2@gmail.com>
2022-04-17 14:23:49 +0530

    assignment

---
+++ block-BNaaei/country-states/models/State.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var stateSchema = new Schema(
+  {
+    name: String,
+    country: [{ type: Schema.Types.ObjectId, ref: 'Country' }],
+    population: Number,
+    neighbouring_states: [{ type: Schema.Types.ObjectId, ref: 'State' }],
+    area: Number,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('State', stateSchema);
